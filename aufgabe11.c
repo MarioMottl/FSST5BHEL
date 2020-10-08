f006fec5bb7db022fa3f6cd8cb2f6d3a90061aa1
MarioMottl <mariomottl17@gmail.com>
2020-10-09 01:58:38 +0200

    Added Comments

--- aufgabe11.c
+++ aufgabe11.c
@@ -6,47 +6,6 @@
 
 int main(int argc, char **argv)
 {
-    pid_t pid,pid2,pid3;
-    pid = fork();
-
-    if (pid == 0)
-    {
-        pid2 = fork();
-        pid3 = fork();
-        if (pid2 == 0)
-        {
-            sleep(5);
-            execl("/usr/bin/chmod","chmod","+x","/tmp/f1",NULL);
-        }
-        else
-        {
-            fprintf (stderr, "Fehler chmod\r\n");
-            exit (1);
-        }
-        if (pid3 == 0)
-        {
-            sleep(10);
-            //find selftouch.c -printf "%m\n"
-            execl("/usr/bin/find","find","/tmp/f1","-printf ","""%m\n""",NULL);
-        }
-        else
-        {
-            fprintf (stderr, "Fehler find\r\n");
-            exit (1);
-        }
-        printf ("Kindprozess:  %d (PID: %d)\n", getpid());
-        execl("/home/mariomottl/Schreibtisch/FSST5BHEL/selftouch","./selftouch","/tmp/f1",NULL);
-    }
-    else if (pid > 0)
-    {
-        int e;
-        waitpid(pid,&e,0);
-        printf ("Elternprozess: %d (PID: %d)\n", getpid());
-    }
-    else
-    {
-        fprintf (stderr, "Fehler");
-        exit (1);
-    }
+    
     return 0;
 }
