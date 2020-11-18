602637d455cfa9cb6a7f0702473bc2aa08f1c701
PrugClem <pdummy10@kabsi.at>
2020-11-18 22:51:56 +0100

    Revert "fixed test template"
    This reverts commit 7b574567ae39425ee9c0025a7aa43c335f521562.

--- Test/main.c
+++ Test/main.c
@@ -8,41 +8,35 @@
 #include <unistd.h>
 #include <errno.h>
 
-typedef int (*child_func_pointer_t)(void*);
+typedef int (*child_func_pointer_t)();
 
-pid_t fork_child_in_function(child_func_pointer_t child_func, const char child_name[], void *arg)
+pid_t fork_child_in_function(child_func_pointer_t child_func, const char child_name[])
 {
     pid_t child_pid = fork();
     if (child_pid == 0)
     {
         printf("child %s  pid=%d (parent_pid=%d)\n", child_name, getpid(), getppid());
-        exit(child_func(arg));
+        exit(child_func());
     }
     if (child_pid < 0)
         printf("Error forking %s!\n", child_name);
     return child_pid;
 }
 
-int wait_child(pid_t child_pid, const char child_name[])
+void wait_child(pid_t child_pid, const char child_name[])
 {
     int child_status;
     waitpid(child_pid, &child_status, 0);
     if (WIFEXITED(child_status))
-    {
         printf("%s has exited with exit code %d\n", child_name, WEXITSTATUS(child_status));
-        return 0;
-    }
     else
-    {
         printf("%s has terminated abnormally\n", child_name);
-        return -1;
-    }
 }
 
-int fork_and_wait_child(child_func_pointer_t child_func, const char child_name[], void *arg)
+void fork_and_wait_child(child_func_pointer_t child_func, const char child_name[])
 {
-    pid_t child_pid = fork_child_in_function(child_func, child_name, arg);
-    return wait_child(child_pid, child_name);
+    pid_t child_pid = fork_child_in_function(child_func, child_name);
+    wait_child(child_pid, child_name);
 }
 
 void createfile(const char filename[])
