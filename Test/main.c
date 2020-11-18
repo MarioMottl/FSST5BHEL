7b574567ae39425ee9c0025a7aa43c335f521562
PrugClem <pdummy10@kabsi.at>
2020-11-18 22:29:48 +0100

    fixed test template added additional parameter for child functions added return values for wait functions fixed missing close() - call

--- Test/main.c
+++ Test/main.c
@@ -8,35 +8,41 @@
 #include <unistd.h>
 #include <errno.h>
 
-typedef int (*child_func_pointer_t)();
+typedef int (*child_func_pointer_t)(void*);
 
-pid_t fork_child_in_function(child_func_pointer_t child_func, const char child_name[])
+pid_t fork_child_in_function(child_func_pointer_t child_func, const char child_name[], void *arg)
 {
     pid_t child_pid = fork();
     if (child_pid == 0)
     {
         printf("child %s  pid=%d (parent_pid=%d)\n", child_name, getpid(), getppid());
-        exit(child_func());
+        exit(child_func(arg));
     }
     if (child_pid < 0)
         printf("Error forking %s!\n", child_name);
     return child_pid;
 }
 
-void wait_child(pid_t child_pid, const char child_name[])
+int wait_child(pid_t child_pid, const char child_name[])
 {
     int child_status;
     waitpid(child_pid, &child_status, 0);
     if (WIFEXITED(child_status))
+    {
         printf("%s has exited with exit code %d\n", child_name, WEXITSTATUS(child_status));
+        return 0;
+    }
     else
+    {
         printf("%s has terminated abnormally\n", child_name);
+        return -1;
+    }
 }
 
-void fork_and_wait_child(child_func_pointer_t child_func, const char child_name[])
+int fork_and_wait_child(child_func_pointer_t child_func, const char child_name[], void *arg)
 {
-    pid_t child_pid = fork_child_in_function(child_func, child_name);
-    wait_child(child_pid, child_name);
+    pid_t child_pid = fork_child_in_function(child_func, child_name, arg);
+    return wait_child(child_pid, child_name);
 }
 
 void createfile(const char filename[])
@@ -70,6 +76,7 @@ void readfromfile(const char filename[],char buffer[],size_t len)
     {
         printf("Error couldnt read from file %s\n", filename);
     }
+    close(fd);
 }
 
 /*
